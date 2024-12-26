d903cee65ee2831f001884323f99d15550dd2d2c
Madhusudhan Reddy <madhuman1012@gmail.com>
2024-12-26 19:54:18 +0530

    Update Pow.cpp

--- Pow.cpp
+++ Pow.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Iterative Solution
 long long power(long long x, int n) {
 	if(x == 1) return 1;
-	int pw = 1;
+	long long pw = 1;
 	while( n != 0) {
 		if(n & 1) pw *= x;
 		x *= x;
